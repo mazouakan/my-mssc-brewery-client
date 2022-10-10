6040b8e552127f64bcb871a3fb09c452e3dd8871
mazoukan <azouakanmed@gmail.com>
2022-10-10 22:50:08 +0200

    Inital Project Add

---
+++ src/main/java/com/azouakan/mymsscbreweryclient/MyMsscBreweryClientApplication.java
@@ -0,0 +1,13 @@
+package com.azouakan.mymsscbreweryclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMsscBreweryClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyMsscBreweryClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/azouakan/mymsscbreweryclient/MyMsscBreweryClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azouakan.mymsscbreweryclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMsscBreweryClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
